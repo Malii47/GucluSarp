f27fd3778f770d43f4d2abd242cff048982a90be
FangeLLL <berkaysoydemirr@gmail.com>
2022-09-10 03:04:27 +0300

    Movement and Dash Improved
    Thanks to my BIG BRAIN I improve our character movement. It is so smooth. Also it can dash now!

---
+++ Sarp_Samuraioglu/Assets/script/PlayerMovement.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rigidBody;
+    public Animator animator;
+    public Camera cam;
+
+    public float activeMoveSpeed;
+    public float dashSpeed;
+
+    public float dashLength =.5f, dashCooldown=1f;
+
+    private float dashCounter;
+    private float dashCoolCounter;
+    private float temp;
+    [SerializeField] private TrailRenderer trailRenderer;
+
+    Vector2 movement;
+    //Vector2 mousePos;
+
+    void Start()
+    {
+        activeMoveSpeed = moveSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+
+        //mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(dashCoolCounter<=0 && dashCounter <= 0)
+            {
+                temp = moveSpeed;
+                activeMoveSpeed = dashSpeed;
+                moveSpeed = activeMoveSpeed;
+                dashCounter = dashLength;
+                trailRenderer.emitting = true;
+            }
+        }
+
+        if (dashCounter > 0)
+        {
+            dashCounter -= Time.deltaTime;
+
+            if (dashCounter <= 0)
+            {
+                moveSpeed = temp;
+                activeMoveSpeed = moveSpeed;
+                dashCoolCounter = dashCooldown;
+                trailRenderer.emitting = false;
+            }
+        }
+
+        if (dashCoolCounter > 0)
+        {
+            dashCoolCounter -= Time.deltaTime;
+        }
+    }
+
+    private void FixedUpdate()
+    {       
+        rigidBody.MovePosition(rigidBody.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+        rigidBody.velocity = movement * moveSpeed;
+
+
+        /*Vector2 lookDir = mousePos - rigidBody.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg -90f;
+        rigidBody.rotation = angle; */
+    }
+}
