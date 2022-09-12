cfb4b8f9420ab96f80cbbd3872ebb5103c9213ed
FangeLLL <berkaysoydemirr@gmail.com>
2022-09-12 03:51:17 +0300

    dumbass stuff fixed

--- Sarp_Samuraioglu/Assets/script/PlayerMovement.cs
+++ Sarp_Samuraioglu/Assets/script/PlayerMovement.cs
@@ -31,6 +31,7 @@ public class PlayerMovement : MonoBehaviour
 
         rigidBody = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        cam = GameObject.FindObjectOfType<Camera>();
 
         activeMoveSpeed = moveSpeed;
     }
