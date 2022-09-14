e253109242f3b2733714843f442edf3d497d7291
Malii47 <Malii47>
2022-09-14 04:24:55 +0300

    Right to Left Katana and Sarp Attack animation done, a few adjestmensts
    31

---
+++ Sarp_Samuraioglu/Assets/script/KatanaAttackAnimation.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KatanaAttackAnimation : MonoBehaviour
+{
+
+
+    public Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator.SetTrigger("isAttack");
+        }
+    }
+}
